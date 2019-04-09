44cce3963ea8b1aeb2babb9fb15c2053ee5dcadd
svc-student <svc-student@student.directory.jccc>
2019-04-09 15:11:10 -0500

    Updated Editor script

--- Assets/Editor/ActorEditor.cs
+++ Assets/Editor/ActorEditor.cs
@@ -6,6 +6,9 @@ using UnityEditor;
 public class ActorEditor : Editor
 {
     bool boardFoldout = false;
+    bool statsFoldout = false;
+    bool attackFoldout = false;
+    bool immunitiesFoldout = false;
 
     public override void OnInspectorGUI()
     {
@@ -35,30 +38,6 @@ public class ActorEditor : Editor
         //Current Target
         newActor.currentTarget = EditorGUILayout.ObjectField("Current Target", newActor.currentTarget, typeof(Actor), true) as Actor;
 
-        //Action Target
-        int actionTargetSelection = 0;
-        string[] actionTargetSelectionOptions = { "Melee Enemy", "Any Enemy", "All Enemy", "Any Ally", "All Ally" };
-        actionTargetSelection = EditorGUILayout.Popup("Action Target", (int)newActor.actionTarget, actionTargetSelectionOptions);
-
-        switch (actionTargetSelection)
-        {
-            case 0:
-                newActor.actionTarget = Actor.ActionTarget.MeleeEnemy;
-                break;
-            case 1:
-                newActor.actionTarget = Actor.ActionTarget.AnyEnemy;
-                break;
-            case 2:
-                newActor.actionTarget = Actor.ActionTarget.AllEnemy;
-                break;
-            case 3:
-                newActor.actionTarget = Actor.ActionTarget.AnyAlly;
-                break;
-            case 4:
-                newActor.actionTarget = Actor.ActionTarget.AllAlly;
-                break;
-        }
-
         //Board Position
         boardFoldout = EditorGUILayout.Foldout(boardFoldout, "Board Position");
         if (boardFoldout)
@@ -69,6 +48,129 @@ public class ActorEditor : Editor
             string[] sideSelections = { "Left", "Right" };
             EditorGUILayout.LabelField("Side Selection");
             sideSelection = GUILayout.SelectionGrid(sideSelection, sideSelections, sideSelections.Length);
+        }
+
+        //Stats
+        statsFoldout = EditorGUILayout.Foldout(statsFoldout, "Stats");
+        if (statsFoldout)
+        {
+            //Max hit points
+            //Setup variables to check if maxHitPoints gets changed
+            bool maxHitChanged = false;
+            int maxHitPrev = newActor.maxHitPoints;
+
+            //Set maxHitPoints
+            newActor.maxHitPoints = EditorGUILayout.IntSlider("Max Hit Points", newActor.maxHitPoints, 0, 1500);
+
+            //Check if maxHitPoints was changed
+            if (maxHitPrev != newActor.maxHitPoints)
+            {
+                maxHitChanged = true;
+            }
+
+            //Hit points
+            //If maxHitPoints was changed, set hitPoints to new max otherwise use current value
+            if (maxHitChanged)
+            {
+                newActor.hitPoints = EditorGUILayout.IntField("Hit Points", newActor.maxHitPoints);
+            }
+            else
+            {
+                newActor.hitPoints = EditorGUILayout.IntField("Hit Points", newActor.hitPoints);
+            }
+
+            //Initiative
+            newActor.initiative = EditorGUILayout.IntSlider("Initiative", newActor.initiative, 10, 100);
+            newActor.initiative = (newActor.initiative / 5) * 5;
+
+            //Damage
+            newActor.damage = EditorGUILayout.IntSlider("Damage", newActor.damage, 0, 180);
+
+            //Percent chance to hit
+            newActor.percentChanceToHit = EditorGUILayout.IntSlider("Percent Chance to Hit", newActor.percentChanceToHit, 0, 100);
+        }
+
+        //Attack information
+        attackFoldout = EditorGUILayout.Foldout(attackFoldout, "Attack");
+        if (attackFoldout)
+        {
+            //Action Target
+            int actionTargetSelection = 0;
+            string[] actionTargetSelectionOptions = { "Melee Enemy", "Any Enemy", "All Enemy", "Any Ally", "All Ally" };
+            actionTargetSelection = EditorGUILayout.Popup("Action Target", (int)newActor.actionTarget, actionTargetSelectionOptions);
+
+            switch (actionTargetSelection)
+            {
+                case 0:
+                    newActor.actionTarget = Actor.ActionTarget.MeleeEnemy;
+                    break;
+                case 1:
+                    newActor.actionTarget = Actor.ActionTarget.AnyEnemy;
+                    break;
+                case 2:
+                    newActor.actionTarget = Actor.ActionTarget.AllEnemy;
+                    break;
+                case 3:
+                    newActor.actionTarget = Actor.ActionTarget.AnyAlly;
+                    break;
+                case 4:
+                    newActor.actionTarget = Actor.ActionTarget.AllAlly;
+                    break;
+            }
+
+            //Action Effect
+            int attackSelection = 0;
+            string[] attackSelectionOptions = { "Normal", "Disable", "Heal"};
+            attackSelection = EditorGUILayout.Popup("Action Effect", (int)newActor.actionEffect, attackSelectionOptions);
+
+            switch (attackSelection)
+            {
+                case 0:
+                    newActor.actionEffect = Actor.ActionEffect.Normal;
+                    break;
+                case 1:
+                    newActor.actionEffect = Actor.ActionEffect.Disable;
+                    break;
+                case 2:
+                    newActor.actionEffect = Actor.ActionEffect.Heal;
+                    break;
+            }
+
+            //Action Effect Source
+            int attackSourceSelection = 0;
+            string[] attackSourceOptions = { "Weapon", "Life", "Death", "Earth", "Air", "Water", "Fire"};
+            attackSourceSelection = EditorGUILayout.Popup("Action Effect Source", (int)newActor.actionEffectSource, attackSourceOptions);
+
+            switch(attackSourceSelection)
+            {
+                case 0:
+                    newActor.actionEffectSource = Actor.ActionSource.Weapon;
+                    break;
+                case 1:
+                    newActor.actionEffectSource = Actor.ActionSource.Life;
+                    break;
+                case 2:
+                    newActor.actionEffectSource = Actor.ActionSource.Death;
+                    break;
+                case 3:
+                    newActor.actionEffectSource = Actor.ActionSource.Fire;
+                    break;
+                case 4:
+                    newActor.actionEffectSource = Actor.ActionSource.Earth;
+                    break;
+                case 5:
+                    newActor.actionEffectSource = Actor.ActionSource.Water;
+                    break;
+                case 6:
+                    newActor.actionEffectSource = Actor.ActionSource.Air;
+                    break;
+            }
+        }
+
+        immunitiesFoldout = EditorGUILayout.Foldout(immunitiesFoldout, "Immunities");
+        if (immunitiesFoldout)
+        {
+
         }
     }
 }
